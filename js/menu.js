4ec2dea545da4029aca5145fb94b3775417801a6
tex3700 <tex3700@gmail.com>
2023-12-23 16:12:33 +0300

    corrected adabtive for site v3

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var header = document.querySelector(".main-nav");
     var svgElement = document.querySelector('.main-logo');
     var langElement = document.querySelector('.languages');
+    var cButton = document.querySelector('.contacts-button');
 
     window.addEventListener("scroll", function() {
         var scrollPosition = window.scrollY;
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
             header.style.background = "rgba(255,255,255,0.8)";
             svgElement.style.backgroundImage = 'url("img/VecNGH1_S%201.svg")'
             langElement.style.display = "none";
+            cButton.style.marginRight = "60px"
             document.querySelectorAll(".main-menu a").forEach(function(item) {
                 item.style.color = "#2F3034";
             });
@@ -18,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
             header.style.background = "none";
             svgElement.style.backgroundImage = 'url("img/VecNGH1_22_263 1.svg")'
             langElement.style.display = "block";
+            cButton.style.marginRight = "10px"
             document.querySelectorAll(".main-menu a").forEach(function(item) {
                 item.style.color = "#FFF";
             });
