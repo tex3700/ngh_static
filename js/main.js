05fa28a2077677feecee5441e77d49adab69bd7b
DmitryP <pylankinds@kenguru.ru>
2024-10-04 18:00:43 +0300

    develop 04/10/24 and media 1500

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+    document.querySelectorAll(".pic").forEach(function (pic) {
+        pic.addEventListener("click", function () {
+            document.querySelectorAll(".pic").forEach(function (pic) {
+                pic.classList.toggle("away"); // Toggle 'away' class on all elements with 'pic' class
+            });
+            this.classList.remove("away"); // Remove 'away' class from the clicked element
+            this.classList.toggle("active"); // Toggle 'active' class on the clicked element
+        });
+    });
 });
