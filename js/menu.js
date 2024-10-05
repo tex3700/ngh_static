b7f132bdcbb955be65fd231755a1d317c5fcbfe4
Dmitry <tex3700@gmail.com>
2024-10-05 23:27:45 +0300

    Develop 05/10/25 final prod correct img

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,33 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    var header = document.querySelector(".main-nav");
-    var svgElement = document.querySelector('.main-logo');
-    var langElement = document.querySelector('.languages');
-    var cButton = document.querySelector('.contacts-button');
+    let header = document.querySelector(".main-nav");
+    let logoElement = document.querySelector('.main-logo');
+    let nameElement = document.querySelector('.main-logo-text');
+    //let logoImage = document.querySelector('.main-logo-img');
+    //let langElement = document.querySelector('.languages');
+    let cButton = document.querySelector('.contacts-button');
 
     window.addEventListener("scroll", function () {
-        var scrollPosition = window.scrollY;
+        let scrollPosition = window.scrollY;
 
         if (scrollPosition > 100) {
             header.style.background = "rgba(255,255,255,0.8)";
-            svgElement.textContent = "";
-            svgElement.style.backgroundImage = 'url("/img/VecNGH1_S%201.svg")'
-            langElement.style.display = "none";
+            nameElement.textContent = "";
+            //logoImage.style.backgroundImage = "unset";
+            logoElement.style.marginTop = "20px";
+            logoElement.style.backgroundImage = 'url(../img/VecNGH1_S_1.svg)';
+            //langElement.style.display = "none";
             cButton.style.marginRight = "60px"
             document.querySelectorAll(".main-menu a").forEach(function (item) {
                 item.style.color = "#2F3034";
             });
         } else {
             header.style.background = "none";
-            svgElement.style.backgroundImage = "unset";
-            svgElement.textContent = "НефтеГазХим";
-            //svgElement.style.backgroundImage = 'url("/img/VecNGH1_22_263 1.svg")'
-            langElement.style.display = "block";
+            logoElement.style.backgroundImage = "unset";
+            logoElement.style.marginTop = "0";
+            nameElement.textContent = "НефтеГазХим";
+            //logoImage.style.backgroundImage = 'url(../img/ngh-logo-colour.png)';
+            //langElement.style.display = "block";
             cButton.style.marginRight = "10px"
             document.querySelectorAll(".main-menu a").forEach(function (item) {
                 item.style.color = "#FFF";
